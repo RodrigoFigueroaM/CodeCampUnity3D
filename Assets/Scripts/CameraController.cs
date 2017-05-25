714f9c14cc986a7d2c810117a03a9a3262850326
rodrigo figueroa <ruimonroy@csu.fullerton.edu>
2017-05-24 21:49:11 -0700

    stuff needed for workshop

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+	public GameObject go;
+	private Vector3 distance;
+
+	void Start ()
+	{
+	}
+
+	void LateUpdate ()
+	{
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	public string tag;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody> ();
+	}
+
+	void FixedUpdate ()
+	{
+
+	}
+
+	void OnTriggerEnter( Collider other)
+	{
+
+	}	
+}
